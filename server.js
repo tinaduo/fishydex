ef6269238230984c324c36dcd063bb6e2ad9c8e5
Skailodon <jackiehtruong@outlook.com>
2025-03-11 21:24:10 -0700

    last push before implementing prs

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+const temp_fish = [
+  {
+    name: "angler",
+    number: 0,
+    description: "Lorem Ipsum",
+    type: "Fish",
+    hp: "1",
+    atk: "1",
+    def: "1",
+  },
+];
 
-app.use(express.static('public'));
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 
 // index page
-app.get('/', function(req, res) {
-  res.render('pages/index');
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+app.get("/fish", function (req, res) {
+  const user = {
+    skills: ["a", "b", "c"],
+  };
+
+  res.render("pages/fish", { user });
 });
 
 app.listen(8080);
-console.log('Server is listening on port 8080');
+console.log("Server is listening on port 8080");
